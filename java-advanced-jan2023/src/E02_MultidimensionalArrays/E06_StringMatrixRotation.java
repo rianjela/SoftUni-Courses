2621ce45d95fe2836792b6cadb4bdaccd45925e2
Ralitsa Angelova <rianjela@gmail.com>
2023-01-22 11:10:49 +0200

    String Matrix Rotation
    Exercises

--- java-advanced-jan2023/src/E02_MultidimensionalArrays/E06_StringMatrixRotation.java
+++ java-advanced-jan2023/src/E02_MultidimensionalArrays/E06_StringMatrixRotation.java
@@ -39,35 +39,104 @@ public class E06_StringMatrixRotation {
             input = scanner.nextLine();
         }
 
-        char[][] matrix = new char[rowsCounter][maxLength];
-        fillMatrixWithList(matrix, wordsList);
-
         //rotation of the matrix / (rotationDegrees / 90 % 4)
         int rotationSteps = degreesRotation / 90 % 4;
 
-        // System.out.println();
+        char[][] matrix;
+        //fill matrix horizontally or vertically
+        if (rotationSteps % 2 != 0) {
+            matrix = new char[maxLength][rowsCounter];
+            fillMatrixWithSpaces(matrix);
+        } else {
+            matrix = new char[rowsCounter][maxLength];
+            fillMatrixWithSpaces(matrix);
+        }
+
+        //rotate and fill
+        switch (rotationSteps) {
+            case 1: // 90 degrees
+                fillMatrix90Deg(matrix, wordsList);
+                break;
+            case 2: // 180 degrees
+                fillMatrix180Deg(matrix, wordsList);
+                break;
+            case 3: // 270 degrees
+                fillMatrix270Deg(matrix, wordsList);
+                break;
+            case 0: // 0 / 360 degrees
+                fillMatrix0Deg(matrix, wordsList);
+                break;
+        }
+
+        printMatrix(matrix);
 
     }
 
-    //fill the matrix with values from the list with the words (+ " ");
-    private static void fillMatrixWithList(char[][] matrix, List<String> list) {
-
-        for (int row = 0; row < list.size(); row++) {
-            char[] currentWordArr = list.get(row).toCharArray();
-            //if the longest word
-            for (int col = 0; col < currentWordArr.length; col++) {
+    // 0/360 degrees
+    private static void fillMatrix0Deg(char[][] matrix, List<String> list) {
+        for (int row = 0; row < matrix.length; row++) {
+            char[] currentWordArr = list.get(row).toCharArray(); // думата на съответния ред
+            for (int col = 0; col <= list.get(row).length() - 1; col++) {
                 matrix[row][col] = currentWordArr[col];
             }
-
-            //if not the longest word - needs " " at the end
-            if (currentWordArr.length < matrix[0].length) {
-                for (int col = currentWordArr.length; col < matrix[0].length; col++) {
-                    matrix[row][col] = ' ';
-                }
-            }
-
         }
     }
 
+
+    // 270 degrees
+    private static void fillMatrix270Deg(char[][] matrix, List<String> list) {
+        for (int col = 0; col <= matrix[0].length - 1; col++) {
+            char[] currentWordArr = list.get(col).toCharArray(); // думата на съответния ред
+            int counterRows = 0;
+            for (int row = matrix.length - 1; row >= matrix.length - list.get(col).length(); row--, counterRows++) {
+                matrix[row][col] = currentWordArr[counterRows];
+            }
+        }
+    }
+
+    // 180 degrees
+    private static void fillMatrix180Deg(char[][] matrix, List<String> list) {
+        int counterRow = 0;
+        for (int row = matrix.length - 1; row >= 0; row--, counterRow++) {
+            char[] currentWordArr = list.get(counterRow).toCharArray(); // думата на съответния ред
+            int counter = 0;
+            for (int col = matrix[0].length - 1; col > matrix[0].length - 1 - list.get(counterRow).length(); col--, counter++) {
+                matrix[row][col] = currentWordArr[counter];
+            }
+        }
+    }
+
+    // 90 degrees
+    private static void fillMatrix90Deg(char[][] matrix, List<String> list) {
+        int counter = 0;
+        for (int col = matrix[0].length - 1; col >= 0; col--, counter++) {
+            char[] currentWordArr = list.get(counter).toCharArray(); // думата на съответния ред
+            for (int row = 0; row < list.get(counter).length(); row++) {
+                matrix[row][col] = currentWordArr[row];
+            }
+        }
+    }
+
+
+    private static void printMatrix(char[][] matrix) {
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                System.out.print(matrix[row][col]);
+            }
+            System.out.println();
+        }
+    }
+
+
+    //fill the matrix with values from the list with the words (+ " ");
+    private static void fillMatrixWithSpaces(char[][] matrix) {
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                matrix[row][col] = ' ';
+            }
+        }
+    }
 
 }
---
+++ java-advanced-jan2023/src/E02_MultidimensionalArrays/E07_Crossfire.java
@@ -0,0 +1,11 @@
+package E02_MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class E07_Crossfire {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
