91dba3f400b4d45360467d5344e8eafff7f10cb3
Ralitsa Angelova <rianjela@gmail.com>
2023-01-31 21:44:53 +0200

    Workshop
    Custom Stack and Smart Array

---
+++ java-advanced-jan2023/src/L07_Workshop/Main.java
@@ -0,0 +1,12 @@
+package L07_Workshop;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CustomStack stack = new CustomStack();
+
+
+        System.out.println(stack.pop());
+
+    }
+}
